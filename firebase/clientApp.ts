fb147fd875302dcc5f2fce1d47e23e9e21a3df29
TioSam77 <sammarw@gmail.com>
2025-05-09 10:15:44 -0600

    09/05/2025

--- firebase/clientApp.ts
+++ firebase/clientApp.ts
@@ -3,8 +3,15 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { envCredentials } from "./envConfigurations";
-
-const firebaseConfig = envCredentials();
+const firebaseConfig = {
+    apiKey: "AIzaSyDnyWUYeFZnu_vpvhqjlU2XCjMjCX6TA1s",
+    authDomain: "api-sam-4f725.firebaseapp.com",
+    projectId: "api-sam-4f725",
+    storageBucket: "api-sam-4f725.firebasestorage.app",
+    messagingSenderId: "129939381722",
+    appId: "1:129939381722:web:eee1a80e1b544a7c3434e3",
+    measurementId: "G-Q25R7XSSD8"
+  };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
@@ -12,3 +19,5 @@ const auth = getAuth(app);
 const db = getFirestore(app);
 
 export { app, auth, db };
+
+console.log(envCredentials())
