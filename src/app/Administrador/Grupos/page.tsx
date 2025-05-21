d238764c602858e321706c232835711b2dbdd50a
IO420 <320154041@pcpuma.acatlan.unam.mx>
2025-05-21 14:11:08 -0400

    fixed repiter

--- src/app/Administrador/Grupos/page.tsx
+++ src/app/Administrador/Grupos/page.tsx
@@ -1,53 +1,6 @@
-'use client'
-import MapCourse from "@/app/componets/course/MapCourse";
-import { onAuthStateChanged } from "firebase/auth";
-import { useEffect, useState } from "react";
-import { auth, db } from '@/../firebase/clientApp';
-import { collection, getDocs, limit, query } from "firebase/firestore";
-
-interface data{
-    id:string
-    name:string
-}
 
 export default function Page() {
-    const [data, setData] = useState<data[]>([]);
-    const [login, setLogin] = useState<boolean>(false)
-    const [notFound, setNotFound] = useState(false);
-
-    useEffect(() => {
-        const unsubscribe = onAuthStateChanged(auth, async (user) => {
-            if (!user) {
-                setData([]);
-                return;
-            }
-
-            try {
-                setLogin(true);
-                const q = query(collection(db, "course"), limit(9));
-                const querySnapshot = await getDocs(q);
-
-                const allData: data[] = querySnapshot.docs.map((doc) => {
-                    const docData = doc.data();
-                    return {
-                        id: doc.id,
-                        name: docData.name,
-                    };
-                });
-
-                setData(allData);
-                setNotFound(allData.length === 0);
-            } catch (err) {
-                console.error("Error al obtener cursos:", err);
-                setNotFound(true);
-            } finally {
-                setLogin(false);
-            }
-        });
-
-        return () => unsubscribe();
-    }, []);
     return (
-        <MapCourse data={data} login={login} notFound={notFound} />
+        <></>
     )
 }
