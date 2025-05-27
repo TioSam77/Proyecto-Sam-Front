59811b23f0a93899b4092998164df5c1bef33f0c
TioSam77 <sammarw@gmail.com>
2025-05-27 17:57:43 -0600

    Editar hotarios

--- src/app/Administrador/page.tsx
+++ src/app/Administrador/page.tsx
@@ -1,7 +1,12 @@
 import Record from "../componets/Record";
+import EditSchedule from "./EditSchedule";
 
 export default function Page() {
     return (
-        <Record />
+        <>
+            <Record />
+            <EditSchedule/>
+        </>
+
     )
 }
