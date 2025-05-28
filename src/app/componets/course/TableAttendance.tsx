dcaf37e81c5d53671e13fb5e49075d7b991bbb7b
IO420 <320154041@pcpuma.acatlan.unam.mx>
2025-05-28 18:52:27 -0400

    add modal

--- src/app/componets/course/TableAttendance.tsx
+++ src/app/componets/course/TableAttendance.tsx
@@ -392,7 +392,7 @@ const TableAttendance = () => {
                 </table>
             </div>
 
-            {showModal && (
+            {showModal && (//arregal esto
                 <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
                     <div className="bg-white p-4 rounded-lg max-w-lg w-full relative">
                         <button
